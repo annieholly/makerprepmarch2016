5645ad3dd69ccead795ed981eea8bcadf7d704c6
Annie Ho <girlybit@gmail.com>
2016-03-24 20:23:17 -0700

    modified example.js

--- example.js
+++ example.js
@@ -1,1 +1,14 @@
 //here is my first commit (NOT)
+
+//commands learned
+//git status
+//git add
+	//git add filename
+	//git add . //this adds all files
+//git commit 
+	//commit message - press "i" to start typing
+	// messages typically written in the present imperative tense
+	// esc, :wq
+//git push origin master 
+//git pull origin master //pulls new changes from Github
+
